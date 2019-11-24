c5c6331484131b1c540e4300c466d861c0f0c57e
yotamb <yotamb@wix.com>
2019-11-24 23:36:52 +0200

    human play mode

--- src/App.svelte
+++ src/App.svelte
@@ -15,19 +15,94 @@
         playGame
 	} from './game'
 
-    let state = createInitialState()
+    let mode = 'human'
 
+    $: {
+        if (mode) { // reset when mode changes
+            state = createInitialState()
+        }
+    }
+
+    let pendingMoves = null
+    let selectedPosition = -1
+
+    let state = createInitialState()
+    let moves
+    $: {
+        moves = getAllValidMoves(state)
+        selectedPosition = -1
+    }
     function playTurn2() {
 	    if (state.phase === GAME_OVER || state.phase === WINNER) {
 	        state = createInitialState()
 	        return
-	    }
-	    const moves = getAllValidMoves(state)
+        }
+        if (mode === 'human') {
+            return
+        }
+
         const action = moves[_.random(moves.length - 1)]
         state = playTurn(state, action)
     }
 
-	$: nextCard = state.deck[state.deck.length - 1]
+    $: nextCard = state.deck[state.deck.length - 1]
+
+    const isValid = position => !!_.find(moves, ({index, index1, index2}) =>
+        index === position || index1 === position || index2 === position
+    )
+
+    const onBoardClick = ({currentTarget}) => {
+        if (state.phase === GAME_OVER || state.phase === WINNER) {
+	        return
+        }
+        
+        const { target } = event
+        const position = parseInt(currentTarget.dataset.position, 10)
+        const card = state.board[position]
+        if (state.phase === REMOVE_CARDS) {
+            if (!card) {
+                return
+            }
+
+            if (parseInt(card.value, 10) === 10) {
+                state = playTurn(state, {
+                    type: REMOVE_CARDS,
+                    index1: position
+                })
+
+                return
+            }
+
+            if (selectedPosition > -1) {
+                const action = _.find(moves, ({index1, index2}) =>
+                    index1 === position && index2 === selectedPosition ||
+                    index1 === selectedPosition && index2 === position
+                )
+
+                if (action) {
+                    state = playTurn(state, action)
+                    return
+                } else {
+                    selectedPosition = -1
+                }
+            }
+            
+            if (isValid(position)) {
+                selectedPosition = position
+            }
+        } else if (state.phase === PLACE_CARD) {
+            const action = _.find(moves, ({index}) => {
+                return index === position
+            })
+
+            if (action) {
+                state = playTurn(state, action)
+            }
+        }
+    }
+
+    const img = new Image()
+    img.src = 'https://dok7xy59qfw9h.cloudfront.net/587/070/202/-239996995-1t62joi-8mq5akftktd5se5/original/file.jpg'
 </script>
 
 <style>
@@ -44,7 +119,28 @@
 	    width: 306px;
 	    height: 406px;
 	    margin: auto;
+        position: relative;
 	}
+
+    .grid.disabled:after {
+        content: '';
+        background: url(https://dok7xy59qfw9h.cloudfront.net/587/070/202/-239996995-1t62joi-8mq5akftktd5se5/original/file.jpg);
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-position: -50px center;
+    }
+
+    .playmode-wrapper {
+        user-select: none;
+        margin: 50px auto;
+        text-align: center;
+    }
+
+    .playmode-wrapper > label {
+        display: inline-block;
+    }
+
 	.next-card {
         margin: 50px auto;
 	}
@@ -52,16 +148,51 @@
         width: 69px;
         height: 94px;
     }
+
+    .placeholder {
+        cursor: pointer;
+        border-radius: 2px;
+    }
+
+    .placeholder:hover {
+        box-shadow: 0px 0px 2px 4px #20b825;
+    }
+
+    .placeholder.invalid:hover {
+        box-shadow: 0px 0px 2px 4px #b82020;
+    }
+
+    .selected {
+        box-shadow: 0px 0px 2px 4px #eee251;
+    }
 </style>
 
 <h1>Play Kadosh! {state.phase}</h1>
 
-<div class="grid">
-    {#each state.board as card}
-    <Card {card} showCard={true}></Card>
+<div class="grid" class:disabled={state.phase === GAME_OVER}>
+    {#each state.board as card, i (i)}
+        <div class="card placeholder" 
+            class:selected={selectedPosition === i}
+            class:invalid={!isValid(i)}
+            data-position={i} 
+            on:click={onBoardClick}>
+            <Card {card} showCard={true} />
+        </div>
     {/each}
 </div>
 
+<div class="playmode-wrapper">
+    Play Mode:
+    <label>
+        <input type=radio bind:group={mode} value={'human'}>
+        Human
+    </label>
+    <label>
+        <input type=radio bind:group={mode} value={'random'}>
+        Random Monkey
+    </label>
+</div>
+
 <div class="card next-card" on:click={playTurn2}>
-    <Card card={nextCard} showCard={state.phase !== REMOVE_CARDS}></Card>
+    <Card card={nextCard} showCard={state.phase === PLACE_CARD}></Card>
 </div>
