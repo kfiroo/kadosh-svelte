ae9e21f06a5822725a7fa0edbf7a516f9d61d56a
kfiroo <kfirg@wix.com>
2019-12-13 02:46:09 +0200

    add firebase performance

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,10 +2,12 @@ import {firebase} from '@firebase/app';
 import '@firebase/analytics'
 import '@firebase/firestore'
 import '@firebase/auth'
+import "firebase/performance";
 
 import firebaseConfig from './firebaseConfig.json'
 
 firebase.initializeApp(firebaseConfig)
+firebase.performance();
 
 export {firebase}
 export const auth = firebase.auth()
